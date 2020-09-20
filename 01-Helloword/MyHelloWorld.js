4f2878e7c4ba43c9ac3d6e8eaf846de47f5fef42
debasish.pradhan <debasish.it.12@gmail.com>
2020-09-21 03:58:06 +0530

    first commit

---
+++ 01-Helloword/MyHelloWorld.js
@@ -0,0 +1,10 @@
+console.log("hello World!");
+var nameIs = "debasisha";
+var age = 31;
+var isMarried = false;
+age = 12;
+var anyvaribale;
+anyvaribale = "never recomonded";
+console.log(nameIs + " " + age + " " + isMarried + " " + anyvaribale);
+//templet string
+console.log(nameIs + " " + age + " " + isMarried + " " + anyvaribale);
---
+++ 01-Helloword/MyHelloWorld.ts
@@ -0,0 +1,11 @@
+console.log("hello World!");
+
+let nameIs: String="debasisha";
+let age: Number=31;
+let isMarried: Boolean=false;
+age=12;
+let anyvaribale: any;
+anyvaribale = "never recomonded";
+console.log(nameIs + " "+age + " "+isMarried+" "+anyvaribale);
+//templet string
+console.log(`${nameIs} ${age} ${isMarried} ${anyvaribale}`);
