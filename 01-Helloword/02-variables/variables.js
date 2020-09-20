191e2844102c42a9316d19f262e4de9ed6e1e850
debasish.pradhan <debasish.it.12@gmail.com>
2020-09-21 04:11:13 +0530

    added variables in typescript

---
+++ 01-Helloword/02-variables/variables.js
@@ -0,0 +1,9 @@
+var nameIs = "debasisha";
+var age = 31;
+var isMarried = false;
+age = 12;
+var anyvaribale;
+anyvaribale = "never recomonded";
+console.log(nameIs + " " + age + " " + isMarried + " " + anyvaribale);
+//templet string
+console.log(nameIs + " " + age + " " + isMarried + " " + anyvaribale);
--- 01-Helloword/MyHelloWorld.ts
+++ 01-Helloword/MyHelloWorld.ts
@@ -1,11 +1,1 @@
 console.log("hello World!");
-
-let nameIs: String="debasisha";
-let age: Number=31;
-let isMarried: Boolean=false;
-age=12;
-let anyvaribale: any;
-anyvaribale = "never recomonded";
-console.log(nameIs + " "+age + " "+isMarried+" "+anyvaribale);
-//templet string
-console.log(`${nameIs} ${age} ${isMarried} ${anyvaribale}`);
